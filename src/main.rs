9f00655fe093e226ac6dba73e18e2b2296f261ae
Shintaroh2004 <watanabe20040802@gmail.com>
2023-10-31 19:59:19 +0900

    静的ファイルを返す

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use actix_files::NamedFile;
-use actix_web::{get, web::ServiceConfig, Responder};
+use actix_files as fs;
+use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
+use actix_files::NamedFile;
+use actix_web::{get,Responder};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -10,7 +12,7 @@ async fn index() -> impl Responder {
 #[shuttle_runtime::main]
 async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(index);
+        cfg.service(fs::Files::new("/", "assets/").show_files_listing());
     };
     Ok(config.into())
 }
